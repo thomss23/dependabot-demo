af2d72547041160ba7723d00fffdcee27fed5cfb
samohtsepiol <borbelithomas@gmail.com>
2024-06-21 18:25:40 +0300

    set up project

---
+++ src/main/java/com/example/dependabotdemo/dependabot_demo/DependabotdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dependabotdemo.dependabot_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DependabotdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DependabotdemoApplication.class, args);
+	}
+
+}
